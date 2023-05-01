55206a59c87b0b9573f70daff471a668be752c9b
Hossein Alizadeh <40279197+Alizadeh128@users.noreply.github.com>
2023-05-01 19:57:06 +0430

    new programs

---
+++ QueraProblemSet(Behdash va Salamat).cs
@@ -0,0 +1,33 @@
+﻿//https://quera.org/problemset/51865/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Grade = Convert.ToInt32(Console.ReadLine());
+            int Days = Convert.ToInt32(Console.ReadLine());
+            int NewGrade = Grade - Days;
+
+            if (Days == 7)
+                System.Console.WriteLine(Grade);
+            else if (Days == 0)
+                System.Console.WriteLine("20");
+            else if (NewGrade < 0)
+                System.Console.WriteLine("0");
+            else
+                System.Console.WriteLine(NewGrade);
+
+
+
+
+        }
+    }
+}
---
+++ QueraProblemSet(Komak be Kapi).cs
@@ -0,0 +1,24 @@
+﻿//https://quera.org/problemset/8838/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Input = Console.ReadLine().Split(' ').Select(a => a.Trim()).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            int Temp = Convert.ToInt32(Input[0]);
+
+            for (int i = 0; i < Temp; i++)
+                System.Console.Write("copy of ");
+            
+            System.Console.Write(Input[1]);
+        }
+    }
+}
